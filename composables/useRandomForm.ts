2260c238f99cadf26d6f4bc2327529532c77601f
GaburaisuVGC <matthieu.barbe01@gmail.com>
2025-09-13 13:12:02 +0200

    fix: restore missing random feature (post-test correction)

--- composables/useRandomForm.ts
+++ composables/useRandomForm.ts
@@ -2,7 +2,7 @@ import { useFetch } from 'nuxt/app'
 import type { RandomFormResponse } from '../types'
 
 export const useRandomForm = async () => {
-  const { data, error } = await useFetch<RandomFormResponse>('/api/random', { server: true })
+  const { data, error } = await useFetch<RandomFormResponse>('/api/random', { server: false })
   return {
     data,
     error
